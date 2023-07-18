969ce8acb3152b339d4b0c5e74aada0126ce741b
Hieu <hieulm.data@gmail.com>
2023-07-18 21:22:16 +0700

    Modify clockGUI

--- clockGUI.py
+++ clockGUI.py
@@ -1,8 +1,6 @@
 import PySimpleGUI as sg
 
-layout = []
-
-window = sg.Window(layout, title="Countdown Clock",  
-          margins=(600, 400), background_color='black', resizable=True, icon='icon.ico')
+window = sg.Window(layout=[[]], title="Countdown Clock",  
+          margins=(600, 400), background_color='black', resizable=True, icon='Material/clockGUI/icon.ico')
 
 window.read()
