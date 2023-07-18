83dd9c4642621d8faf80f9a079d1bfaa0fe42bdc
Hieu <hieulm.data@gmail.com>
2023-07-18 21:37:47 +0700

    Rename

--- clockGUI.py
+++
@@ -1,6 +0,0 @@
-import PySimpleGUI as sg
-
-window = sg.Window(layout=[[]], title="Countdown Clock",  
-          margins=(600, 400), background_color='black', resizable=True, icon='Material/clockGUI/icon.ico')
-
-window.read()
---
+++ countdown_clock_gui.py
@@ -0,0 +1,6 @@
+import PySimpleGUI as sg
+
+window = sg.Window(layout=[[]], title="Countdown Clock",  
+          margins=(600, 400), background_color='black', resizable=True, icon='Material/clockGUI/icon.ico')
+
+window.read()
