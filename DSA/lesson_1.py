0f9811cd4f32534e038a37ebb670b6da4c6e50d1
Hieu <hieulm.data@gmail.com>
2023-07-21 15:12:37 +0700

    ok

--- DSA/lesson_1.py
+++ DSA/lesson_1.py
@@ -23,8 +23,6 @@ def print_odd(a): # in ra cac so chan
     for x in a:
         if x%2 == 0:
             print(x)
-        
-           
 
 def print_even(a): # in ra cac so le
     for x in a:
@@ -35,13 +33,27 @@ def print_larger_x(a, x): # in ra cac so lon hon x
     for b in a:
         if b > x:
             print(b)
-        
+            
+def test(a):
+    a.append(2) # them vao cuoi
+    del a[1] # xoa phan tu a[1]
+    a.insert(3,50) # o index them gia tri
+    
+def swap(a, x, y): # doi cho gia tri 2 index x va y
+    pass
+
+def find_max(a): # tim gia tri lon nhat
+    pass
+
+def find_min(a): # tim gia tri nho nhat
+    pass
+
+def check_element(a): # kiem tra xem x co o trong list khong
+    pass
+
+def del_list(a): # xoa toan bo list
+    pass
+      
 if __name__ == "__main__":
-    print_larger_x(a,3)
-def print_larger_1(a): # in ra cac so lon hon x
-    for x in a:
-        if x > 1:
-            print(x)
+   test(a)
         
-if __name__ == "__main__":
-    print_larger_1(a)
