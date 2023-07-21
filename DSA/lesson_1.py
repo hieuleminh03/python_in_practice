cbafb5be27e7aaad46d60412a4d6c3dd98b62bb0
Hieu <hieulm.data@gmail.com>
2023-07-21 14:33:11 +0700

    ok

---
+++ DSA/lesson_1.py
@@ -0,0 +1,32 @@
+a = [1,2,3,4,5,5,6]
+
+def check_odd(a): # in ra tinh chan le cua tung so ?
+    for x in a:
+        if x%2 == 0:
+            print("Chan")
+        else:
+            print("Le")
+            
+def sum(a): # tinh tong
+    s = 0
+    for x in a:
+        s += x
+    print("Tong la: " + str(s))
+    
+def tich(a): # tinh tich
+    s = 1
+    for x in a:
+        s *= x
+    print("Tich la: " + str(s))
+    
+def print_odd(a): # in ra cac so chan
+    pass
+
+def print_even(a): # in ra cac so le
+    pass
+
+def print_larger_x(a, x): # in ra cac so lon hon x
+    pass 
+        
+if __name__ == "__main__":
+    tich(a)
