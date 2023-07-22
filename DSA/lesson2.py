19a4f9d9b1ae50fef9d4472c901f8b38445a1d84
Hieu <hieulm.data@gmail.com>
2023-07-22 15:06:45 +0700

    lesson 2

---
+++ DSA/lesson2.py
@@ -0,0 +1,23 @@
+# String
+
+def concat(): # Nhập vào 2 chuỗi, ghép lại rồi in ra
+    # VD: nor mal  ->  normal
+    pass
+
+def separate(): # Nhập vào 1 chuỗi, in ra từng chữ cái của nó
+    # VD: okb  ->
+    # o
+    # k
+    # b
+    pass
+
+def count_char(): # Nhập vào 1 chuỗi, in ra số chữ cái nó có
+    # VD: example -> 7
+    pass
+
+def capitalize(): # Nhập vào 1 chuỗi, in ra nó nhưng tất cả viết hoa
+    # VD: vrSCbg -> VRSCBG
+    pass
+
+if __name__ == "__main__":
+    pass
