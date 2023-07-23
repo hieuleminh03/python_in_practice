f39cb87538f3e09ebe62af8ec1e69cee09e221ae
Hieu <hieulm.data@gmail.com>
2023-07-23 23:47:00 +0700

    add lesson 3

---
+++ DSA/lesson_3.py
@@ -0,0 +1,18 @@
+# lesson 3
+
+def check_palindrome(): # Nhap vao 1 chuoi, kiem tra xem chuoi do co phai la palindrome ko
+    # xyz ko phai palindrome
+    # xyx -> xyx
+    # abba -> abba
+    # neu la palindrome print("yes") ko thi print("no")
+    a = input("Nhap chuoi: ")
+    pass
+
+def reverse(): # nhap vao 1 chuoi, in ra chuoi do viet nguoc lai
+    # abc -> cba
+    # 0xyyx -> xyyx0
+    a = input("Nhap chuoi: ")
+    pass
+
+if __name__ == "__main__":
+    check_palindrome()
