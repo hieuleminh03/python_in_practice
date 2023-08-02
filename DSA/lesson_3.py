ad0cf6d5ab86ae050ba70da5770a7cd359f269f3
Hieu <hieulm.data@gmail.com>
2023-08-02 20:42:30 +0700

    ok

--- DSA/lesson_3.py
+++ DSA/lesson_3.py
@@ -7,16 +7,43 @@ def check_palindrome(): # Nhap vao 1 chuoi, kiem tra xem chuoi do co phai la pal
     # neu la palindrome print("yes") ko thi print("no")
     # goi y: su dung ham len(a) de nhan ve do dai cua chuoi
     a = input("Nhap chuoi: ")
-    pass
+    b = reverse(a)
+    if a==b:
+        print("ok")
+    else:
+        print("!!")
 
 def reverse(): # nhap vao 1 chuoi, in ra chuoi do viet nguoc lai
     # abc -> cba
     # 0xyyx -> xyyx0
     a = input("Nhap chuoi: ")
-    pass
+    l = len(a)
+    b = ""
+    # zux
+    # l = 3
+    # i in (0,2)
+    # i = 0 -> b+a[2] -> b = "x"
+    # i = 1 -> b+a[1] -> b = "xu"
+    # i = 2 -> b+a[0] -> b = "xuz"
+    for i in range(0, l):
+        b = b+a[l-i-1]
+    return b
+
+def reverse(a):
+    l = len(a)
+    b = ""
+    # zux
+    # l = 3
+    # i in (0,2)
+    # i = 0 -> b+a[2] -> b = "x"
+    # i = 1 -> b+a[1] -> b = "xu"
+    # i = 2 -> b+a[0] -> b = "xuz"
+    for i in range(0, l):
+        b = b+a[l-i-1]
+    return b
 
 def test():
     a = input("Chuoi: ")
 
 if __name__ == "__main__":
-    test()
+    check_palindrome()
